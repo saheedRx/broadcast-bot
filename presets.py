11adea34a009bc6f9a9a721c8883d825c4ab14cf
saheedRx <89293737+saheedRx@users.noreply.github.com>
2022-03-05 18:11:05 +0530

    Update presets.py

--- presets.py
+++ presets.py
@@ -1,5 +1,5 @@
 class Presets(object):
-    WELCOME_MESSAGE = "Hello"
+    WELCOME_MESSAGE = "Hey send Spotify Song/Track Link to Download.\n\n More @SAHEEDS_BOTS "
     USERS_LIST = "<b>Total:</b>\n\nSubscribers - {}\nBlocked / Deleted - {}"
     WAIT_MSG = "<b>Please Wait...</b>"
     REPLY_ERROR = "<code>Use this command as a reply to any telegram message with out any spaces.</code>"
