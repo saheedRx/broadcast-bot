7ccb722e72ee65368391e011cde1eae8d2effcc9
saheedRx <89293737+saheedRx@users.noreply.github.com>
2022-03-09 14:30:38 +0530

    Update presets.py

--- presets.py
+++ presets.py
@@ -1,5 +1,5 @@
 class Presets(object):
-    WELCOME_MESSAGE = "<b>Hello</b>"
+    WELCOME_MESSAGE = "<b>:hi Enter Movie/Series Name</b>"
     USERS_LIST = "<b>Total:</b>\n\nSubscribers - {}\nBlocked / Deleted - {}"
     WAIT_MSG = "<b>Please Wait...</b>"
     REPLY_ERROR = "<code>Use this command as a reply to any telegram message with out any spaces.</code>"
